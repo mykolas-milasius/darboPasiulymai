2a47ca1edf9fea246f2785303afa52efb9d0fdb6
mykolas-milasius <mykolas.milasius@gmail.com>
2022-06-22 14:11:36 +0300

    Sutvarkytas error'as su main.css loadinimu

--- darbopasiulymai/src/main/java/projektas/darbopasiulymai/WebSecurityConfig.java
+++ darbopasiulymai/src/main/java/projektas/darbopasiulymai/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/").permitAll()
+				.antMatchers("/","/main.css","/images/company_logo.png").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
